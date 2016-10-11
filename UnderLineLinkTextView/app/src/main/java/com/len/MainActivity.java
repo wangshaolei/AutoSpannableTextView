eacad4d485473ba0057b8700b509f7bcf196553c
len <huiyitt@126.com>
2016-10-11 11:31:19 +0800

    v2.0
    v2.0

--- UnderLineLinkTextView/app/src/main/java/com/len/MainActivity.java
+++ UnderLineLinkTextView/app/src/main/java/com/len/MainActivity.java
@@ -1,8 +1,10 @@
 package com.len;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import com.len.library.AutoLinkStyleTextView;
 
 public class MainActivity extends AppCompatActivity {
     private AutoLinkStyleTextView autoLinkStyleTextView;
